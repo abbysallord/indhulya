a7e152e2a567d85a5e84e07d5527986a621e9860
abbysallord <dshenoyh@gmail.com>
2026-06-13 19:42:08 +0530

    chore: change favicon to Indhulya logo

--- frontend/components/SmoothScroll.tsx
+++ frontend/components/SmoothScroll.tsx
@@ -1,10 +1,26 @@
 "use client";
-import { ReactLenis } from "lenis/react";
+import { useEffect } from "react";
+import Lenis from "lenis";
 
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
-  return (
-    <ReactLenis root options={{ lerp: 0.05, duration: 1.5, smoothWheel: true }}>
-      {children}
-    </ReactLenis>
-  );
+  useEffect(() => {
+    const lenis = new Lenis({
+      lerp: 0.05,
+      duration: 1.5,
+      smoothWheel: true,
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
+  return <>{children}</>;
 }
